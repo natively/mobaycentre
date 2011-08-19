f72b2559fb667157f5f47e5d6aabd90ccba38768
Alex Lewis <ebcomalex@gmail.com>
2011-08-19 09:30:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobaycentre::Application.config.secret_token = '149a693fe8991149ad30e61f70aaf670b8333964e68085326a75e2a9eabce0ca30e27454745b7966b7f37a02d704cf1fd331265d0c62d7b9bc884947193deb49'
