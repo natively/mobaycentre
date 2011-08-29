bf8f3a5bb98c5968a37fd60250237df0a2c104a8
Alex Lewis <ebcomalex@gmail.com>
2011-08-29 11:10:44 -0400

    randomly selects an ad TODO: add a \'in rotation\' feature to the ads engine

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
+jQuery.jQueryRandom = 0;
+jQuery.extend(jQuery.expr[":"],
+{
+    random: function(a, i, m, r) {
+        if (i == 0) {
+            jQuery.jQueryRandom = Math.floor(Math.random() * r.length);
+        };
+        return i == jQuery.jQueryRandom;
+    }
+});
+
 $(document).ready(function() {
-  $('.ads'). RANDOM . show();
+  $('.ads:random').show();
 });
