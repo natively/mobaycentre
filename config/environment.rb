1c7ff355e3ecccc691491d6ff567ce16791479d2
Brian Chia <briancchia@gmail.com>
2011-10-25 10:29:00 -0400

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Mobaycentre::Application.initialize!
+
+config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.sendgrid.net',
+    :port           => '25',
+    :authentication => :plain,
+    :user_name      => 'briancchia',
+    :password       => 'jaebzzzz',
+    :domain         => 'mobaycentre.com'
+  } 
