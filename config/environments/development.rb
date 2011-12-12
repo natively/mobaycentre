79faa1060bfc5a547177297c444776e9a33e03a4
Alex Lewis <ebcomalex@gmail.com>
2011-12-12 13:55:31 -0500

    image gallery! it's ALLLIIIIVEE

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ end
 Refinery.rescue_not_found = false
 # When true will use Amazon's Simple Storage Service on your production machine
 # instead of the default file system for resources and images
+ENV['S3_KEY']='AKIAII7JWM4STRAD3NNA'
+ENV['S3_SECRET']='9kzqB/m0MY3xddoUAREvXS0WbIQpnMJS8a9etkmB'
+ENV['S3_BUCKET']='mobaycentre'
 Refinery.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
