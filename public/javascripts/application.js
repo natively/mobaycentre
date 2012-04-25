8015cf0fcab5eb1dbd4fb80d22ca2b22569b714e
Alex Lewis <ebcomalex@gmail.com>
2012-04-25 11:00:13 -0400

    analytics... and a bit of cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,16 @@
+
+/* GOOGLE ANALYTICS */
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25297656-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+/* JQUERY EXTENSION */
 jQuery.jQueryRandom = 0;
 jQuery.extend(jQuery.expr[":"],
 {
@@ -9,6 +22,7 @@ jQuery.extend(jQuery.expr[":"],
     }
 });
 
+/* Okay, let's get on with it */
 $(document).ready(function(){
   $('.ads:random').show();
   
